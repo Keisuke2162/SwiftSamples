055b5c798e0bde7fbb698df1c3ce22079c3ca614
Keisuke2162 <applabo.kei@gmail.com>
2024-03-12 03:01:01 +0900

    Initial Commit

---
+++ SwiftSamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSamples
+//
+//  Created by Kei on 2024/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSamples/SwiftSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSamplesApp.swift
+//  SwiftSamples
+//
+//  Created by Kei on 2024/03/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
