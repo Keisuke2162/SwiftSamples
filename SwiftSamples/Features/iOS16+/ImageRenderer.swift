579999a304cef96460d4f7aa360de0cbf7205f68
Keisuke2162 <applabo.kei@gmail.com>
2024-05-12 22:07:40 +0900

    ファイル整理

--- SwiftSamples/Features/iOS16+/ImageRenderer.swift
+++ SwiftSamples/Features/iOS16+/ImageRenderer.swift
@@ -8,7 +8,41 @@
 /// https://developer.apple.com/documentation/swiftui/imagerenderer
 
 import ComposableArchitecture
+import Foundation
 import SwiftUI
+
+@Reducer
+struct ImageRendererReducer {
+    @ObservableState
+    struct State: Equatable {
+        var imageCenter: CGPoint?
+        var backgroundSize: CGSize?
+    }
+
+    enum Action {
+        case tapped(CGPoint)
+        case tapSaveImageButton(UIImage?)
+        case getBackgroundSize(CGSize)
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case let .tapped(point):
+                state.imageCenter = point
+                return .none
+            case let .tapSaveImageButton(image):
+                if let saveImage = image {
+                    UIImageWriteToSavedPhotosAlbum(saveImage, nil, nil, nil)
+                }
+                return .none
+            case let .getBackgroundSize(size):
+                state.backgroundSize = size
+                return .none
+            }
+        }
+    }
+}
 
 struct ImageRendererView: View {
     @Bindable var store: StoreOf<ImageRendererReducer>
