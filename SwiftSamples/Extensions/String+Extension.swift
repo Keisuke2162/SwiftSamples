a964e07078164728ffceb2bf89446935249e7f51
Keisuke2162 <applabo.kei@gmail.com>
2024-11-17 21:44:36 +0900

    add extension

--- SwiftSamples/Extensions/String+Extension.swift
+++ SwiftSamples/Extensions/String+Extension.swift
@@ -1,7 +1,19 @@
-//
-//  String+Extension.swift
-//  SwiftSamples
-//
-//  Created by Kei on 2024/11/17.
-//
+import Foundation
 
+extension String {
+  public func toDate() -> Date? {
+    let dateFormatter = ISO8601DateFormatter()
+    return dateFormatter.date(from: self)
+  }
+
+  // 文字列から数値のみ取り出す
+  public func extractNumericPart() -> String {
+    let pattern = #"^\d+(\.\d+)?|\d+(?=%)"#
+    let regex = try? NSRegularExpression(pattern: pattern)
+    
+    if let match = regex?.firstMatch(in: self, range: NSRange(self.startIndex..., in: self)) {
+      return String(self[Range(match.range, in: self)!])
+    }
+    return ""
+  }
+}
